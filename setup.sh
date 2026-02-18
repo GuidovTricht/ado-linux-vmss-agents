a8d85f156d154bc04edf48aebb70454c91a0f921
Guido van Tricht <guido.van.tricht@valtech.com>
2026-02-17 18:06:37 -0800

    Auto approve apt install

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ echo "Starting setup at $(date)"
 
 # Add Docker's official GPG key:
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -30,6 +30,6 @@ EOF
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Setup completed at $(date)"
